9634457e6aa94d67fd3f0160809c48d522631682
edwin-bjorlin <edwin@bjorlin.io>
2023-05-19 08:31:00 +0000

    -

--- main.js
+++ main.js
@@ -16,3 +16,53 @@ function validateFunctions(...functionNames) {
         throw unsupportedFunctions;
     }
 }
+
+function enableFeatureIfSupported(featureName, featureFunction) {
+    try {
+        featureFunction();
+    } catch {
+        console.log(featureName + " is disabled because " + err);
+    }
+}
+
+function imagePopUp() {
+    validateFunctions(features.querySelector, features.querySelectorAll);
+
+    const projects = document.querySelector(".section--projects");
+
+    if (projects === null) {
+        return;
+    }
+    
+    const projectImages = projects.querySelectorAll("img");
+
+    if (projectImages === null) {
+        return;
+    }
+
+    projectImages.forEach(image => {
+        image.style.cursor = "zoom-in";
+
+        image.addEventListener("click", image => {
+            const divTag = document.createElement("div");
+            divTag.classList.add("container--image-pop-up");
+            divTag.style.cursor = "zoom-out";
+
+            divTag.addEventListener("click", _ => {
+                const popUpContainer = document.querySelector(".container--image-pop-up");
+                popUpContainer.remove();
+            })
+
+            const imageTag = document.createElement("img");
+            imageTag.src = image.target.src.replace("--thumbnail", "");
+
+            divTag.appendChild(imageTag);
+
+            const bodyTag = document.querySelector("body");
+            bodyTag.appendChild(divTag);
+            
+        });
+    });
+}
+
+enableFeatureIfSupported("image pop up", imagePopUp);
